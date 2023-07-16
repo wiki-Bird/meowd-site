1e471b92b6007503b29f28ce25a2f267d0510305
KwiiHours <41028518+KwiiHours@users.noreply.github.com>
2023-07-17 09:54:17 +1200

    Updated ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# meowd-site
-The SvelteKit version of the Meowd website
+<p align="center">
+    <a href="https://meowd.ramiels.me/" target="_blank" rel="noreferrer noopener">
+        <img width="170" alt="Logo" src="https://media.discordapp.net/attachments/669394205705240606/1095216676154392677/lgoo1.png">
+    </a>
+    <h1 align="center">Meowd Website<br/><br/></h1>
+</p>
+
+
+### Meowd Site
+
+The Meowd website Svelte source\
+View the live site at <https://meowd.ramiels.me/>
+
+---
+
+### Meowd Bot
+
+View the Meowd bot at <https://github.com/wiki-Bird/meowd>
--- meowdSiteSveltekit/src/routes/guide/+page.svelte
+++ meowdSiteSveltekit/src/routes/guide/+page.svelte
@@ -14,7 +14,7 @@
             <a href="/features" class="nav">Features</a>
             <a href="/commands" class="nav">Commands</a>
             <a href="/guide" class="nav" id="selected">Guide</a>
-            <a href="https://discord.com/oauth2/authorize?client_id=1086188667678244955&scope=bot&permissions=8" class="nav" id="addBtn">INVITE</a>
+            <a href="https://discord.com/oauth2/authorize?client_id=1086188667678244955&scope=bot&permissions=8" target="_blank" rel="noreferrer" class="nav" id="addBtn">INVITE</a>
         </div>
     </div>
     <div class="mainContent">
