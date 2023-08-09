59bbac8eb8f55073784d42fa627f643beede217c
KwiiHours <41028518+KwiiHours@users.noreply.github.com>
2023-08-09 21:16:48 +1200

    Added 404

---
+++ meowdSiteSveltekit/src/routes/404/+page.svelte
@@ -0,0 +1,26 @@
+
+<style>
+    @import '/src/main.css';
+    @import '/src/subpage.css';
+</style>
+
+<main>
+    <div class="navbar">
+        <a href="/" class="nav" id="brand"><div class="branding">
+            <img src="/lgoo1.png" class="logoHeader" alt="Meowd Logo" /> 
+            MEOWD
+        </div></a>
+        <div class="links">
+            <a href="/features" class="nav">Features</a>
+            <a href="/commands" class="nav">Commands</a>
+            <a href="/guide" class="nav">Guide</a>
+            <a href="https://discord.com/oauth2/authorize?client_id=1086188667678244955&scope=bot&permissions=8" target="_blank" rel="noreferrer" class="nav" id="addBtn">INVITE</a>
+        </div>
+    </div>
+    <div class="mainContent">
+        <h3>404</h3>
+        <h2>Page not found D:</h2>
+        <br>
+        <img src="/404.png" class="img404" alt="meowd going bwah"/>
+    </div>
+</main>
